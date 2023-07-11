2a42909d0cd3e2aa93be738c6f676c271db48141
Sandesh Pokhrel <sandesh.pokhrel33@gmail.com>
2023-07-11 11:20:33 +0545

    CRUD implementation using Nest

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('hello')
+  @HttpCode(201)
+  create(): string {
+    return 'Created post';
+  }
+}
