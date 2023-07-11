85cc569239bacffb929d4afecc776a1a8adfb079
Sandesh Pokhrel <sandesh.pokhrel33@gmail.com>
2023-07-11 12:04:17 +0545

    validationpipe added

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -24,7 +24,6 @@ export class ArticlesService {
   }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
     return this.prisma.article.update({
       where: { id },
       data: updateArticleDto,
