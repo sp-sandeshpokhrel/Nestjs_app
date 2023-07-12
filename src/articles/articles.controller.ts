2cf9034ccf3af68aacb738b3b652129eb0ac412a
Sandesh Pokhrel <sandesh.pokhrel33@gmail.com>
2023-07-12 10:18:27 +0545

    added user schema with one to many realtion with article

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,3 +1,4 @@
+// src/articles/articles.controller.ts
 import {
   Controller,
   Get,
@@ -6,7 +7,6 @@ import {
   Patch,
   Param,
   Delete,
-  NotFoundException,
   ParseIntPipe,
 } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
@@ -22,43 +22,46 @@ export class ArticlesController {
 
   @Post()
   @ApiCreatedResponse({ type: ArticleEntity })
-  create(@Body() createArticleDto: CreateArticleDto) {
-    return this.articlesService.create(createArticleDto);
+  async create(@Body() createArticleDto: CreateArticleDto) {
+    return new ArticleEntity(
+      await this.articlesService.create(createArticleDto),
+    );
   }
 
   @Get()
   @ApiOkResponse({ type: ArticleEntity, isArray: true })
-  findAll() {
-    return this.articlesService.findAll();
+  async findAll() {
+    const articles = await this.articlesService.findAll();
+    return articles.map((article) => new ArticleEntity(article));
   }
 
   @Get('drafts')
   @ApiOkResponse({ type: ArticleEntity, isArray: true })
-  findDrafts() {
-    return this.articlesService.findDrafts();
+  async findDrafts() {
+    const drafts = await this.articlesService.findDrafts();
+    return drafts.map((draft) => new ArticleEntity(draft));
   }
 
   @Get(':id')
   @ApiOkResponse({ type: ArticleEntity })
   async findOne(@Param('id', ParseIntPipe) id: number) {
-    const article = await this.articlesService.findOne(id);
-    if (!article) {
-      throw new NotFoundException(`Article #${id} not found`);
-    }
-    return article;
+    return new ArticleEntity(await this.articlesService.findOne(id));
   }
 
   @Patch(':id')
-  update(
+  @ApiCreatedResponse({ type: ArticleEntity })
+  async update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateArticleDto: UpdateArticleDto,
   ) {
-    return this.articlesService.update(id, updateArticleDto);
+    return new ArticleEntity(
+      await this.articlesService.update(id, updateArticleDto),
+    );
   }
 
   @Delete(':id')
   @ApiOkResponse({ type: ArticleEntity })
-  remove(@Param('id', ParseIntPipe) id: number) {
-    return this.articlesService.remove(id);
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    return new ArticleEntity(await this.articlesService.remove(id));
   }
 }
--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -2,6 +2,7 @@
 
 import { Article } from '@prisma/client';
 import { ApiProperty } from '@nestjs/swagger';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 export class ArticleEntity implements Article {
   @ApiProperty()
@@ -24,4 +25,18 @@ export class ArticleEntity implements Article {
 
   @ApiProperty()
   updatedAt: Date;
+
+  @ApiProperty({ required: false, nullable: true })
+  authorId: number | null;
+
+  @ApiProperty({ required: false, type: UserEntity })
+  author?: UserEntity;
+
+  constructor({ author, ...data }: Partial<ArticleEntity>) {
+    Object.assign(this, data);
+
+    if (author) {
+      this.author = new UserEntity(author);
+    }
+  }
 }
