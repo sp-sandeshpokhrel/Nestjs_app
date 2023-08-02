97e1bda7993cac7f9ab51d6f3fcd4e2779e23b4f
Sandesh Pokhrel <sandesh.pokhrel33@gmail.com>
2023-08-02 08:44:31 +0545

    readme updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,17 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { CacheManagerModule } from './utils/cache/cache.module';
 
 @Module({
   imports: [
-    CacheManagerModule,
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     ArticlesModule,
     UsersModule,
     AuthModule,
+    CacheManagerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
